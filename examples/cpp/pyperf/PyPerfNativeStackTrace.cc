79395c7f17807bd850ff62913bd5c2110c57713e
Izabella Raulin <izabella.raulin@intel.com>
2022-09-13 18:45:57 +0200

    Added caching in PyperfNativeStack

--- examples/cpp/pyperf/PyPerfNativeStackTrace.cc
+++ examples/cpp/pyperf/PyPerfNativeStackTrace.cc
@@ -8,11 +8,12 @@
 #include <sys/uio.h>
 #include <errno.h>
 #include <unistd.h>
-#include <cxxabi.h>
-
 #include <cstdio>
 #include <cstring>
 #include <sstream>
+#include <time.h>     
+#include <chrono>
+#include <thread>
 
 #include "PyPerfLoggingHelper.h"
 
@@ -20,10 +21,13 @@ namespace ebpf {
 namespace pyperf {
 
 // Ideally it was preferable to save this as the context in libunwind accessors, but it's already used by UPT
+const uint16_t NativeStackTrace::CacheMaxSizeMB = 56;
+const uint16_t NativeStackTrace::CacheMaxTTL = 300; // In seconds
 const uint8_t *NativeStackTrace::stack = NULL;
 size_t NativeStackTrace::stack_len = 0;
 uintptr_t NativeStackTrace::sp = 0;
 uintptr_t NativeStackTrace::ip = 0;
+MAP NativeStackTrace::cache;
 
 NativeStackTrace::NativeStackTrace(uint32_t pid, const unsigned char *raw_stack,
                                    size_t stack_len, uintptr_t ip, uintptr_t sp) : error_occurred(false) {
@@ -43,26 +47,47 @@ NativeStackTrace::NativeStackTrace(uint32_t pid, const unsigned char *raw_stack,
   // The UPT implementation of these functions uses ptrace. We want to make sure they aren't getting called
   my_accessors.access_fpreg = NULL;
   my_accessors.resume = NULL;
-
-  int res;
-  unw_addr_space_t as = unw_create_addr_space(&my_accessors, 0);
-  void *upt = _UPT_create(pid);
-  if (!upt) {
-    this->symbols.push_back(std::string("[Error _UPT_create (system OOM)]"));
-    this->error_occurred = true;
-    goto out;
-  }
-
+  unw_addr_space_t as;
   unw_cursor_t cursor;
-  // TODO: It's possible to make libunwind use cache using unw_set_caching_policy, which might lead to significent
-  //       performance improvement. We just need to make sure it's not dangerous. For now the overhead is good enough.
-  res = unw_init_remote(&cursor, as, upt);
-  if (res) {
-    std::ostringstream error;
-    error << "[Error unw_init_remote (" << unw_strerror(res) << ")]";
-    this->symbols.push_back(error.str());
-    this->error_occurred = true;
-    goto out;
+  void *upt;
+  int res;
+
+  // Pseudo-proactive way of implementing TTL - whenever any call is made, all expired entries are removed
+  cache_eviction(cache);
+
+  // Check whether the entry for the process ID is presented in the cache
+  if (is_cached(cache, pid) == false) {
+    logInfo(2,"The given key %d is not presented in the cache\n", pid);
+   
+    as = unw_create_addr_space(&my_accessors, 0);
+    upt = _UPT_create(pid);
+    if (!upt) {
+      this->symbols.push_back(std::string("[Error _UPT_create (system OOM)]"));
+      this->error_occurred = true;
+      cleanup(upt, as); 
+      return;
+    }
+
+    res = unw_init_remote(&cursor, as, &upt);  
+    if (res) {
+      std::ostringstream error;
+      error << "[Error unw_init_remote (" << unw_strerror(res) << ")]";
+      this->symbols.push_back(error.str());
+      this->error_occurred = true;
+      cleanup(upt, as); 
+      return;
+    }
+    
+    // Put to the cache
+    cache_put(cache, pid, cursor, as, upt);
+
+  } else {
+    logInfo(2,"Found entry for the given key %d in the cache\n", pid);
+    // Get from the cache
+    Object cached_value = cache_get(cache, pid);
+    cursor = cached_value.cursor;
+    as = cached_value.as;
+    upt = cached_value.upt;
   }
 
   do {
@@ -107,7 +132,9 @@ NativeStackTrace::NativeStackTrace(uint32_t pid, const unsigned char *raw_stack,
     }
   } while (unw_step(&cursor) > 0);
 
-out:
+}
+
+void NativeStackTrace::cleanup(void *upt, unw_addr_space_t as) {
   if (upt) {
     _UPT_destroy(upt);
   }
@@ -198,6 +225,96 @@ std::vector<std::string> NativeStackTrace::get_stack_symbol() const {
 
 bool NativeStackTrace::error_occured() const {
   return error_occurred;
+}
+
+bool NativeStackTrace::is_cached(const MAP &map, const uint32_t &key) {
+  try {
+      map.at(key);
+      return true;
+  }
+  catch (const std::out_of_range&) {
+      logInfo(2, "No entry for %d in the cache\n", key);
+  }
+  return false;
+}
+
+Object NativeStackTrace::cache_get(const MAP &map, const uint32_t &key) {
+  const Object & value = map.at(key);
+  return value;
+}
+
+// cache_put adds a new entry to the map if the capacity allows
+void NativeStackTrace::cache_put(MAP &map, const uint32_t &key, const unw_cursor_t cursor, const unw_addr_space_t as, void *upt) {
+  
+  // Check available capacity
+  if (cache_size() > NativeStackTrace::CacheMaxSizeMB*1024*1024 - cache_single_entry_size()) { 
+    logInfo(2, "The cache usage is %.2f MB, close to reaching the max memory usage (%d MB)\n", cache_size_KB()/1024, NativeStackTrace::CacheMaxSizeMB);
+    logInfo(2, "Skipping adding an entry for %d to the cache\n", key);
+     
+    return;
+  }
+
+  Object obj = {cursor, as, upt, time(NULL)};
+  map[key] = obj;
+  logInfo(2, "New entry for %d was added to the cache\n", key);
+}
+
+// cache_delete_key removes the element from the cache and destroys unwind address space and UPT
+// to ensure that all memory and other resources are freed up
+bool NativeStackTrace::cache_delete_key(MAP &map, const uint32_t &key) {
+  try {
+    Object v = cache_get(map, key);    
+    map.erase(key);
+    cleanup(v.upt, v.as);
+    logInfo(2, "The entry for %d was deleted from the cache\n", key);
+    return true;
+  }
+  catch (const std::out_of_range&) {
+    logInfo(2, "Failed to delete entry for %d: no such key in the cache\n", key);
+  }
+  return false;
+}
+
+// cache_single_entry_size returns the number of bytes taken by single entry
+uint32_t NativeStackTrace::cache_single_entry_size() const {  
+  return sizeof(decltype(cache)::key_type) + sizeof(decltype(cache)::mapped_type);
+}
+
+// cache_size returns the number of bytes currently in use by the cache
+uint32_t NativeStackTrace::cache_size() const {  
+  return sizeof(cache) + cache.size()*cache_single_entry_size();
+}
+
+// cache_size_KB returns the number of kilobytes currently in use by the cache
+float NativeStackTrace::cache_size_KB() const {  
+  return cache_size()/1024;
+}
+
+// cache_eviction removes elements older than 5 minutes (CacheMaxTTL=300)
+void NativeStackTrace::cache_eviction(MAP &map) {
+  std::vector<uint32_t> keys_to_delete;
+  float _prev_cache_size = cache_size_KB();
+
+  for(std::map<uint32_t, Object>::iterator iter = map.begin(); iter != map.end(); ++iter)
+  {
+    uint32_t k =  iter->first;
+    const Object & v =  iter->second;
+    if (std::abs(difftime(v.timestamp, time(NULL))) > NativeStackTrace::CacheMaxTTL) {
+      // exceeding TTL
+      keys_to_delete.push_back(k);
+    }
+  }
+
+  // Delete expired entries
+  for( size_t i = 0; i < keys_to_delete.size(); i++ ) {
+      cache_delete_key(map, keys_to_delete[i]);
+  }
+
+  if (keys_to_delete.size() > 0) {
+    float _cache_size = cache_size_KB();
+    logInfo(2,"Evicted %d item(s) from the cache\n", keys_to_delete.size());
+    logInfo(2,"The cache usage after eviction action is %.2f KB (released %.2f KB)\n", _cache_size, _prev_cache_size - _cache_size);    
+  }
 }
 
 }  // namespace pyperf
--- examples/cpp/pyperf/PyPerfNativeStackTrace.h
+++ examples/cpp/pyperf/PyPerfNativeStackTrace.h
@@ -8,9 +8,19 @@
 
 #include <string>
 #include <vector>
+#include <map>
 
 namespace ebpf {
 namespace pyperf {
+
+typedef struct {
+  unw_cursor_t cursor;
+  unw_addr_space_t as;
+  void *upt;
+  time_t timestamp;
+} Object;
+
+typedef std::map<uint32_t, Object> MAP;
 
 class NativeStackTrace {
  public:
@@ -19,7 +29,6 @@ class NativeStackTrace {
 
   std::vector<std::string> get_stack_symbol() const;
   bool error_occured() const;
-
  private:
   std::vector<std::string> symbols;
   bool error_occurred;
@@ -29,11 +38,27 @@ class NativeStackTrace {
   static uintptr_t ip;
   static uintptr_t sp;
 
+  static MAP cache;
+  static const uint16_t CacheMaxSizeMB;
+  static const uint16_t CacheMaxTTL;
+
+  uint32_t cache_size() const; 
+  uint32_t cache_single_entry_size() const;
+  float cache_size_KB() const; 
+    
   static int access_reg(unw_addr_space_t as, unw_regnum_t regnum,
                         unw_word_t *valp, int write, void *arg);
 
   static int access_mem(unw_addr_space_t as, unw_word_t addr, unw_word_t *valp,
                         int write, void *arg);
+
+  void cleanup(void *upt, unw_addr_space_t as);
+
+  bool is_cached(const MAP &map, const uint32_t &key);
+  void cache_put(MAP &map, const uint32_t &key, const unw_cursor_t cursor, const unw_addr_space_t as, void *upt);
+  Object cache_get(const MAP &map, const uint32_t &key);
+  bool cache_delete_key(MAP &map, const uint32_t &key);
+  void cache_eviction(MAP &map);
 };
 
 }  // namespace pyperf
